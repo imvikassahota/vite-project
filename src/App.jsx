e3a86c94d97a439e35c52e46d72f7c0f08bb4bea
imvikassahota <imvikassahota@gmail.com>
2025-09-03 01:14:01 +0530

    Added routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-  // return <Login />;
-  return <Register />
+  return (
+    <BrowserRouter>
+      {/* Routers */}
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
